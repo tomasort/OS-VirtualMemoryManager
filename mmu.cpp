c264c682c94ba72cbb915f2e9dba732f4ade41a6
Tomas Ortega Rojas <tor213@linserv1.cims.nyu.edu>
2021-04-13 04:22:19 -0400

    adding files

---
+++ mmu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <list>
+#include <iomanip>
+
+
+using namespace std;
+
+// TODO: All lines starting with '#'  must be ignored 
+
+int main(int argc, char* argv[]){
+    // TODO: user getopt to find the flags, input file and output file. 
+    cout << "Hello world" << endl;
+    return 0;
+}
