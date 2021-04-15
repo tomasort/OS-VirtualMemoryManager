8aa7d2ce30841bdec468d31b37beaa8f50c63ac3
Tomas Ortega <tomasortega.cs@gmail.com>
2021-04-15 03:47:53 -0400

    adding cmake file for clion

---
+++ cmake-build-debug/CMakeFiles/os_lab3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_lab3.dir/mmu.cpp.o"
+  "os_lab3"
+  "os_lab3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/os_lab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/os_lab3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for os_lab3.
+# This may be replaced when dependencies are built.
